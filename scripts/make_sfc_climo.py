ce0a4b0f07985b05cd67a6ce09b0750dc13753a7
Naureen <naureen.bharwani@noaa.gov>
2024-09-20 16:09:06 +0000

    Rework make_sfc_climo script

--- scripts/make_sfc_climo.py
+++ scripts/make_sfc_climo.py
@@ -26,7 +26,7 @@ def link_files(dest_dir, files):
             no_halo_fn = fn.replace("halo4.", "")
             for link in (fn, no_halo_fn):
                 link = Path(link)
-                if (linkname := fix_lam_path / link.name).is_symlink():
+                if (linkname := dest_dir / link.name).is_symlink():
                     linkname.unlink()
                 linkname.symlink_to(path)
     
@@ -36,15 +36,13 @@ def link_files(dest_dir, files):
             tile1_fn = fn.replace("tile7.halo0", "tile1")
             for link in (fn, tile1_fn):
                 link = Path(link)
-                if (linkname := fix_lam_path / link.name).is_symlink():
+                if (linkname := dest_dir / link.name).is_symlink():
                     linkname.unlink()
                 linkname.symlink_to(path)
-    
-    Path(rundir / "make_sfc_climo_task_complete.txt").touch()
 
 
 parser = ArgumentParser(
-    description="Script that runs SfcClimoGen via uwtools API",
+    description="Script that runs SfcClimoGen via uwtools API.",
 )
 parser.add_argument(
     "-c",
@@ -56,13 +54,14 @@ parser.add_argument(
 )
 parser.add_argument(
     "--key-path",
-    help="Dot-separated path of keys leading through the config to the driver's YAML block",
+    help="Dot-separated path of keys leading through the config to the driver's YAML block.",
     metavar="KEY[.KEY...]",
     required=True,
+    type=lambda s: s.split("."),
 )
 
-
 args = parser.parse_args()
+
 expt_config = get_yaml_config(args.config_file)
 CRES = expt_config["workflow"]["CRES"]
 os.environ["CRES"] = CRES 
@@ -73,27 +72,36 @@ expt_config.dereference(
     }
 )
 
+args = parser.parse_args()
 
-# Run sfc_climo_gen 
-sfc_climo_gen_driver = SfcClimoGen(
-    config=expt_config,
-    key_path=[args.key_path],
-)
-rundir = Path(sfc_climo_gen_driver.config["rundir"])
-print(f"Will run sfc_climo_gen in {rundir}")
-sfc_climo_gen_driver.run()
+def make_sfc_climo(config_file, key_path):
+    # Run sfc_climo_gen 
+    sfc_climo_gen_driver = SfcClimoGen(
+        config=config_file,
+        key_path=key_path,
+    ) 
+    rundir = Path(sfc_climo_gen_driver.config["rundir"])
+    print(f"Will run sfc_climo_gen in {rundir}")
+    sfc_climo_gen_driver.run()
+    
+    if not (rundir / "runscript.sfc_climo_gen.done").is_file():
+        print("Error occurred running sfc_climo_gen. Please see component error logs.")
+        sys.exit(1)
+    
+    
+    # Deliver output data
+    fix_lam_path = Path(expt_config["workflow"]["FIXlam"])
+    link_files(
+        dest_dir=fix_lam_path,
+        files=glob.glob(str(rundir / f"*.nc")),
+    )
+    
+    Path(rundir / "make_sfc_climo_task_complete.txt").touch()
 
-if not (rundir / "runscript.sfc_climo_gen.done").is_file():
-    print("Error occurred running sfc_climo_gen. Please see component error logs.")
-    sys.exit(1)
 
+if __name__ == "__main__":
 
-# Deliver output data
-
-fix_lam_path = Path(expt_config["workflow"]["FIXlam"])
-link_files(
-    dest_dir=fix_lam_path,
-    files=glob.glob(str(rundir / f"*.nc")),
-)
-
-Path(rundir / "make_sfc_climo_task_complete.txt").touch()
+    make_sfc_climo(
+        config_file=args.config_file,
+        key_path=args.key_path,
+    )
