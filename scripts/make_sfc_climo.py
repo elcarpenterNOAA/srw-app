bb740f8b5a1d51e42dbe54ea13c621edf047168d
Naureen <naureen.bharwani@noaa.gov>
2024-09-23 21:03:21 +0000

    Wrap parse_args into function

--- scripts/make_sfc_climo.py
+++ scripts/make_sfc_climo.py
@@ -14,7 +14,7 @@ from uwtools.api.sfc_climo_gen import SfcClimoGen
 from uwtools.api.config import get_yaml_config
 
 
-def _link_files(dest_dir, files):
+def _link_files(dest_dir, files, cres):
     """
     Link a given list of files to the destination directory updating the file names.
     """
@@ -23,7 +23,7 @@ def _link_files(dest_dir, files):
         fn = Path(fpath).name
     
         if "halo" in fn:
-            fn = f"{CRES}.{(fn.replace('halo', 'halo4'))}"
+            fn = f"{cres}.{(fn.replace('halo', 'halo4'))}"
             no_halo_fn = fn.replace("halo4.", "")
             for link in (fn, no_halo_fn):
                 link = Path(link)
@@ -33,7 +33,7 @@ def _link_files(dest_dir, files):
     
         else:
             basename = path.stem
-            halo0_fn = f"{CRES}.{basename}.halo0.nc"
+            halo0_fn = f"{cres}.{basename}.halo0.nc"
             tile1_fn = halo0_fn.replace("tile7.halo0", "tile1")
             for link in (halo0_fn, tile1_fn):
                 link = Path(link)
@@ -41,45 +41,45 @@ def _link_files(dest_dir, files):
                     linkname.unlink()
                 linkname.symlink_to(path)
 
-
-parser = ArgumentParser(
-    description="Script that runs sfc_climo_gen via uwtools API.",
-)
-parser.add_argument(
-    "-c",
-    "--config-file",
-    metavar="PATH",
-    required=True,
-    help="Path to experiment config file.",
-    type=Path,
-)
-parser.add_argument(
-    "--key-path",
-    help="Dot-separated path of keys leading through the config to the driver's YAML block.",
-    metavar="KEY[.KEY...]",
-    required=True,
-    type=lambda s: s.split("."),
-)
-
-args = parser.parse_args()
-expt_config = get_yaml_config(args.config_file)
-
-# The experiment config will have {{ CRES | env }} expressions in it that need to be
-# dereferenced during driver initialization
-CRES = expt_config["workflow"]["CRES"]
-os.environ["CRES"] = CRES 
-expt_config.dereference(
-    context={
-        **os.environ,
-        **expt_config,
-    }
-)
+def parse_args(argv):
+    parser = ArgumentParser(
+        description="Script that runs sfc_climo_gen via uwtools API.",
+    )
+    parser.add_argument(
+        "-c",
+        "--config-file",
+        metavar="PATH",
+        required=True,
+        help="Path to experiment config file.",
+        type=Path,
+    )
+    parser.add_argument(
+        "--key-path",
+        help="Dot-separated path of keys leading through the config to the driver's YAML block.",
+        metavar="KEY[.KEY...]",
+        required=True,
+        type=lambda s: s.split("."),
+    )
+    
+    return parser.parse_args(argv)
 
 
 def make_sfc_climo(config_file, key_path):
     """
     Run the sfc_climo_gen driver.
     """
+    expt_config = get_yaml_config(config_file)
+    
+    # The experiment config will have {{ CRES | env }} expressions in it that need to be
+    # dereferenced during driver initialization
+    cres = expt_config["workflow"]["CRES"]
+    os.environ["CRES"] = cres 
+    expt_config.dereference(
+        context={
+            **os.environ,
+            **expt_config,
+        }
+    )
     sfc_climo_gen_driver = SfcClimoGen(
         config=config_file,
         key_path=key_path,
@@ -99,6 +99,7 @@ def make_sfc_climo(config_file, key_path):
     _link_files(
         dest_dir=fix_lam_path,
         files=glob.glob(str(rundir / f"*.nc")),
+        cres=cres,
     )
     
     # Mark the successful completion of the script on disk
@@ -107,6 +108,7 @@ def make_sfc_climo(config_file, key_path):
 
 if __name__ == "__main__":
 
+    args = parse_args(sys.argv[1:])
     make_sfc_climo(
         config_file=args.config_file,
         key_path=args.key_path,
