30c5f391ee2d22d312b7e381fa49cacf9f1a78f3
elcarpenterNOAA <emily.l.carpenter@noaa.gov>
2024-10-09 18:49:10 +0000

    formatting

--- scripts/make_grid.py
+++ scripts/make_grid.py
@@ -3,125 +3,153 @@ The run script for making the grid files for the experiment.
 """
 
 import glob
+import logging
 import os
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
 
-from uwtools.api.drivers import ESGGrid, GlobalEquivResol, MakeHgrid, MakeSoloMosaic, Shave
 from uwtools.api.config import get_yaml_config
+from uwtools.api.esg_grid import ESGGrid
+from uwtools.api.global_equiv_resol import GlobalEquivResol
+from uwtools.api.logging import use_uwtools_logger
+from uwtools.api.make_hgrid import MakeHgrid
+from uwtools.api.make_solo_mosaic import MakeSoloMosaic
+from uwtools.api.shave import Shave
 
-parser = ArgumentParser(
-    description="Script that runs the make_grid task via uwtools API",
-)
-parser.add_argument(
-    "-c",
-    "--config-file",
-    metavar="PATH",
-    required=True,
-    help="Path to experiment config file.",
-    type=Path,
-)
-parser.add_argument(
-    "--key-path",
-    help="Dot-separated path of keys leading through the config to the tasks's YAML block",
-    metavar="KEY[.KEY...]",
-    required=True,
-)
 
-args = parser.parse_args()
-expt_config = get_yaml_config(args.config_file)
-make_grid_config = get_yaml_config(expt_config[args.key_path])
-task_rundir = Path(make_grid_config["rundir"])
-print(f"Will run make_grid in {task_rundir}")
+def link_files(dest_dir, files):
+    """
+    Link a given list of files to the destination directory using the same file names.
+    """
+    for fpath in files:
+        path = Path(fpath)
+        linkname = dest_dir / path.name
+        if linkname.is_symlink():
+            linkname.unlink()
+        logging.info(f"Linking {linkname} -> {path}")
+        linkname.symlink_to(path)
 
-CRES = expt_config["workflow"]["CRES"]
-os.environ["CRES"] = CRES
 
-KEY_PATH = args.key_path.split('.')
-fix_lam_path = Path(expt_config["workflow"]["FIXlam"])
-
-# Run either make_hgrid or esg_grid 
-
-if grid_gen_method == "GFDLgrid":
-    make_hgrid_driver = MakeHgrid(
-        config=args.config_file,
-        key_path=[args.key_path],
+def parse_args(argv):
+    """
+    Parse arguments for the script.
+    """
+    parser = ArgumentParser(
+        description="Script that runs the make_grid task via uwtools API.",
     )
-    rundir = Path(make_hgrid_driver.config["rundir"])
-    print(f"Will run make_hgrid_driver in {rundir}")
-    make_hgrid_driver.run()
-    if not (rundir / "runscript.make_hgrid.done").is_file():
-        print("Error occurred running make_hgrid. Please see component error logs.")
-        sys.exit(1)
-# grid_fn="GFDLgrid.tile7.nc"
-# ELC: if ESGgrid is default, should it simply be "else"? 
-else if grid_gen_method == "ESGgrid":
-    esg_grid_driver = ESGGrid(
-        config=args.config_file,
-        key_path=[args.key_path],
+    parser.add_argument(
+        "-c",
+        "--config-file",
+        metavar="PATH",
+        required=True,
+        help="Path to experiment config file.",
+        type=Path,
     )
-    rundir = Path(esg_grid_driver.config["rundir"])
-    print(f"Will run esg_grid_driver in {rundir}")
-    esg_grid_driver.run()
-
-    if not (rundir / "runscript.esg_grid.done").is_file():
-        print("Error occurred running esg_grid. Please see component error logs.")
-        sys.exit(1)
-# regional_grid.nc in rundir
-# Run global_equiv_resol
-
-global_equiv_resol_driver = GlobalEquivResol(
-    config=args.config_file,
-    key_path=[args.key_path],
-)
-rundir = Path(global_equiv_resol_driver.config["rundir"])
-print(f"Will run global_equiv_resol_driver in {rundir}")
-global_equiv_resol_driver.run()
-
-if not (rundir / "runscript.global_equiv_resol.done").is_file():
-    print("Error occurred running global_equiv_resol. Please see component error logs.")
-    sys.exit(1)
-# Run shave (repeatedly, with different numbers of halo grid cells)
-
-for sub_path in ["shave03", "shave04"]:
-    key_path = KEY_PATH + [sub_path]
-    shave_driver = Shave(
-        config=args.config_file,
-        key_path=KEY_PATH,
+    parser.add_argument(
+        "--key-path",
+        help="Dot-separated path of keys leading through the config to the tasks's YAML block.",
+        metavar="KEY[.KEY...]",
+        required=True,
+        type=lambda s: s.split("."),
     )
-    rundir = Path(shave_driver.config["rundir"])
-    print(f"Will run {sub_path} in {rundir}")
-    shave_driver.run()
-    if not (rundir / "runscript.shave.done").is_file():
-       print(f"Error occurred running {sub_path}. Please see component error logs.")
-       sys.exit(1)
+    return parser.parse_args(argv)
 
-# Link shave output to fix directory
-for fpath in glob.glob(str(task_rundir / f"{CRES}*.nc")):
-    path = Path(fpath)
-    linkname = fix_lam_path / path.name
-    if linkname.is_symlink():
-        linkname.unlink()
-    linkname.symlink_to(path)
 
-# Run make_solo_mosaic
+# args = parser.parse_args()
 
-for sub_path in ["NHW", "NH3", "NH4", "NH0"]:
-    key_path = KEY_PATH + [sub_path]
-    make_solo_mosaic_driver = MakeSoloMosaic(
-        config=args.config_file,
+
+def make_grid(config_file, key_path):
+    """
+    Run the series of drivers needed to produce NetCDF-formatted grid files.
+    """
+    expt_config = get_yaml_config(config_file)
+    make_grid_dict = expt_config
+    for kp in key_path:
+        make_grid_dict = make_grid_dict[kp]
+    make_grid_config = get_yaml_config(make_grid_dict)
+    task_rundir = Path(make_grid_config["rundir"])
+    logging.info(f"Will run make_grid in {task_rundir}")
+
+    # The experiment config will have {{ CRES | env }} expressions in it that need to be
+    # dereferenced during driver initialization.
+    cres = expt_config["workflow"]["CRES"]
+    os.environ["CRES"] = cres
+
+    # Destination of important files from this process
+    fix_lam_path = Path(expt_config["workflow"]["FIXlam"])
+
+    # Run either make_hgrid or esg_grid
+    if grid_gen_method == "GFDLgrid":
+        run_driver(driver_class=MakeHgrid, config_file=config_file, key_path=key_path)
+    # ELC: grid_fn="GFDLgrid.tile7.nc"
+    elif grid_gen_method == "ESGgrid":
+        run_driver(driver_class=ESGGrid, config_file=config_file, key_path=key_path)
+    # ELC: regional_grid.nc in rundir
+
+    # Run global_equiv_resol
+    run_driver(
+        driver_class=GlobalEquivResol, config_file=config_file, key_path=key_path
+    )
+
+    # Run shave for 3- and 4-cell-wide halo
+    for subpath in ["shave3", "shave4"]:
+        driver = run_driver(
+            driver_class=Shave,
+            config_file=config_file,
+            key_path=[*key_path, subpath],
+        )
+
+    # Link shave output to fix directory (shave config points to task dir, not shave rundir)
+    link_files(
+        dest_dir=fix_lam_path,
+        files=glob.glob(str(task_rundir / f"{cres}*.nc")),
+    )
+
+    # Run make_solo_mosaic
+    for subpath in ["NHW", "NH3", "NH4", "NH0"]:
+        driver = run_driver(
+            driver_class=MakeSoloMosaic,
+            config_file=config_file,
+            key_path=[*key_path, subpath],
+        )
+    # Link make-solo_mosaic output to fix directory
+    link_files(
+        dest_dir=fix_lam_path,
+        files=glob.glob(str(task_rundir / f"C*_mosaic.halo{subpath}.nc")),
+    )
+    # Mark the successful completion of the script on disk.
+    Path(task_rundir / "make_grid_task_complete.txt").touch()
+
+
+def run_driver(driver_class, config_file, key_path):
+    """
+    Initialize and run the provided UW driver.
+
+    Return the configured object.
+    """
+    driver = driver_class(
+        config=config_file,
         key_path=key_path,
     )
-    rundir = Path(make_solo_mosaic_driver.config["rundir"])
-    print(f"Will run make_solo_mosaic_driver in {rundir}")
-    make_solo_mosaic_driver.run()
-    linkname = fix_lam_path / "C*_mosaic.halo{sub_path}.nc"
-    if linkname.is_symlink():
-        linkname.unlink()
-    linkname.symlink_to(task_rundir / f"C*_mosaic.halo{subpath}.nc")
-    if not (rundir / "runscript.make_solo_mosaic.done").is_file():
-        print("Error occurred running make_solo_mosaic. Please see component error logs.")
-        sys.exit(1)
+    rundir = Path(driver.config["rundir"])
+    logging.info(f"Will run {driver.driver_name()} in {rundir}")
+    driver.run()
 
-Path(task_rundir / "make_grid_task_complete.txt").touch()
+    if not (rundir / f"runscript.{driver.driver_name()}.done").is_file():
+        logging.error(
+            f"Error occurred running {driver.driver_name()}. Please see component error logs."
+        )
+        sys.exit(1)
+    return driver
+
+
+if __name__ == "__main__":
+
+    use_uwtools_logger()
+
+    args = parse_args(sys.argv[1:])
+    make_grid(
+        config_file=args.config_file,
+        key_path=args.key_path,
+    )
--- ush/setup.py
+++ ush/setup.py
@@ -210,13 +210,15 @@ def load_config_for_setup(ushdir, default_config, user_config):
 
     # Load one more if running Coupled AQM
 
-    if cfg_d['cpl_aqm_parm']['CPL_AQM']:
+    if cfg_d["cpl_aqm_parm"]["CPL_AQM"]:
         cfg_aqm = get_yaml_config(Path(ushdir, "config_defaults_aqm.yaml"))
         update_dict(cfg_aqm, cfg_d)
 
     # Load CCPP suite-specific settings
-    ccpp_suite = cfg_d['workflow']['CCPP_PHYS_SUITE']
-    ccpp_cfg = get_yaml_config(Path(ushdir, "ccpp_suites_defaults.yaml")).get(ccpp_suite, {})
+    ccpp_suite = cfg_d["workflow"]["CCPP_PHYS_SUITE"]
+    ccpp_cfg = get_yaml_config(Path(ushdir, "ccpp_suites_defaults.yaml")).get(
+        ccpp_suite, {}
+    )
     update_dict(ccpp_cfg, cfg_d)
 
     # Set "Home" directory, the top-level ufs-srweather-app directory
